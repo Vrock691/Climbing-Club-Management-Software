b570b4ff1405d0fde93a2008995c06b71cb54c31
Valentin Mary <valentin.mary@etu.univ-amu.fr>
2026-04-01 15:41:27 +0200

    Initial commit

---
+++ src/test/java/fr/mary/berger/climbing/club/manager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mary.berger.climbing.club.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
